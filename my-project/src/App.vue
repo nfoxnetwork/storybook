cf309a23012c572fa77f430b77d2f83618b59cbd
Issei Horie <issei.horie@is2ei.com>
2020-05-10 15:37:54 +0900

    Removed unused code
    Signed-off-by: Issei Horie <issei.horie@is2ei.com>

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,17 +1,34 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div>
+    <h1>Vue.js &amp; Storybook example</h1>
+    <div>
+      <my-button text="Button" />
+    </div>
+    <div>
+      <my-button text="Primary" kind="primary" />
+      <my-button text="Default" kind="default" />
+      <my-button text="Success" kind="success" />
+      <my-button text="Info" kind="info" />
+      <my-button text="Warning" kind="warning" />
+      <my-button text="Danger" kind="danger" />
+      <my-button text="Dark" kind="dark" />
+    </div>
+    <div>
+      <my-button text="Minimum" size="minimum" />
+      <my-button text="Small" size="small" />
+      <my-button text="Normal" size="normal" />
+      <my-button text="Large" size="large" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import MyButton from './components/Button'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyButton
   }
 }
 </script>
